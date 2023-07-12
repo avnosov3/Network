ba6be4a862795b093c887e13276c37dbfb96bd7f
Artem Nosov <nosov1995@gmail.com>
2023-07-12 14:04:47 +0200

    modify patch with extra-forbid

--- app/schemas/post.py
+++ app/schemas/post.py
@@ -1,12 +1,19 @@
 from datetime import datetime
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Extra
 
 
 class PostCreate(BaseModel):
-    text: str = Field(..., min_length=1)
+    text: str
     pub_date: datetime
+
+    class Config:
+        min_anystr_length = 1
 
 
 class PostUpdate(BaseModel):
-    text: str = Field(..., min_length=1)
+    text: str
+
+    class Config:
+        extra = Extra.forbid
+        min_anystr_length = 1
