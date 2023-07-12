f09e5ea0bad74948e456dbd9f16cc11cd25d04e1
Artem Nosov <nosov1995@gmail.com>
2023-07-12 12:23:18 +0200

    add first model: Post

---
+++ app/models/post.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+from sqlalchemy import Column, DateTime, Text
+
+from ..core.db import Base
+
+
+class Post(Base):
+    text = Column(Text, nullable=True)
+    # author
+    pub_date = Column(DateTime, default=datetime.utcnow)
+
+    OUT = ('текст поста {text:.15} дата публикации {pub_date}')
+
+    def __repr__(self):
+        return self.OUT.format(self.text, self.pub_date)
