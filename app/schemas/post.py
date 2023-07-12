71a67387e68e75d3820c9c0df449ef1c73f32d13
Artem Nosov <nosov1995@gmail.com>
2023-07-12 14:00:10 +0200

    check empty text of post

--- app/schemas/post.py
+++ app/schemas/post.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class PostCreate(BaseModel):
-    text: str
+    text: str = Field(..., min_length=1)
     pub_date: datetime
 
 
 class PostUpdate(BaseModel):
-    text: str
+    text: str = Field(..., min_length=1)
