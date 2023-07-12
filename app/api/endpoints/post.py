bb422b540c9fb5e1ed2ccf14dc70ec0d0b2eb84c
Artem Nosov <nosov1995@gmail.com>
2023-07-12 13:23:51 +0200

    add get-all posts

--- app/api/endpoints/post.py
+++ app/api/endpoints/post.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.db import get_async_session
@@ -7,9 +7,22 @@ from app.schemas.post import PostCreate
 
 post_router = APIRouter()
 
+POSTS_NOT_FOUND = 'Посты не найдены'
 
-@post_router.post('/post/')
+
+@post_router.post('/')
 async def create_post(
     post: PostCreate, session: AsyncSession = Depends(get_async_session)
 ):
     return await post_crud.create(post, session)
+
+
+@post_router.get('/')
+async def get_all_posts(session: AsyncSession = Depends(get_async_session)):
+    posts = await post_crud.get_all(session)
+    if posts is None:
+        raise HTTPException(
+            status_code=404,
+            detail=POSTS_NOT_FOUND
+        )
+    return posts
